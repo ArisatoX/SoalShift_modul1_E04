2ee4cc096794b1f3ef6fba026550a3fe43953f01
Ananta Dwi Prasetya Purna Yuda <anantadwi13@yahoo.com>
2019-02-22 09:48:59 +0700

    update soal1

--- soal1.sh
+++ soal1.sh
@@ -1,7 +1,12 @@
-#!bin/bash
+#!/bin/bash
 
-x=1;
-for x in `ls /home/arisatox/Downloads/nature/`
+dir="/home/anantadwi13/SISOP/modul1"
+
+unzip -o "$dir/nature.zip" > /dev/null
+
+mkdir -p "$dir/nature-extracted"
+
+for x in `ls $dir/nature`
 do
-`base64 -d /home/arisatox/Downloads/nature/$x | xxd -r > /home/arisatox/Downloads/tes/$x`
+	`base64 -d "$dir/nature/$x" | xxd -r > "$dir/nature-extracted/$x"`
 done
